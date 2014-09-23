74c9696f7fd4bf6dd5ad6dd804b0e7658588fe57
Xiangrui Meng <meng@databricks.com>
2014-09-22 17:56:46 -0700

    add GridPartitioner

---
+++ src/main/scala/als/GridPartitioner.scala
@@ -0,0 +1,45 @@
+package als
+
+import org.apache.spark.Partitioner
+
+class GridPartitioner(val partitioners: Array[Partitioner]) extends Partitioner {
+
+  require(partitioners.size > 1)
+
+  private val cumProds: Array[Int] = partitioners.map(_.numPartitions).scanLeft(1)(_ * _)
+
+  override val numPartitions: Int = cumProds.last
+
+  override def getPartition(key: Any): Int = {
+    val keys = key.asInstanceOf[Product]
+    require(keys.productArity == partitioners.size)
+    var partition = 0
+    var i = 0
+    while (i < partitioners.size) {
+      partition += partitioners(i).getPartition(keys.productElement(i)) * cumProds(i)
+      i += 1
+    }
+    partition
+  }
+
+  override def equals(other: Any): Boolean = {
+    other match {
+      case o: GridPartitioner =>
+        partitioners.zip(o.partitioners).forall(p => p._1 == p._2)
+      case _ =>
+        false
+    }
+  }
+
+  def indices(partition: Int): Array[Int] = {
+    val output = new Array[Int](partitioners.size)
+    var cur = partition
+    var i = 0
+    while (i < partitioners.size) {
+      output(i) = cur % partitioners(i).numPartitions
+      cur /= partitioners(i).numPartitions
+      i += 1
+    }
+    output
+  }
+}
---
+++ src/test/scala/als/GridPartitionerSuite.scala
@@ -0,0 +1,21 @@
+package als
+
+import org.apache.spark.HashPartitioner
+import org.scalatest.FunSuite
+
+class GridPartitionerSuite extends FunSuite {
+  test("GridPartitioner with two partitioners") {
+    val numParts0 = 10
+    val part0 = new HashPartitioner(numParts0)
+    val numParts1 = 7
+    val part1 = new HashPartitioner(numParts1)
+    val gridPart = new GridPartitioner(Array(part0, part1))
+    assert(gridPart.numPartitions === numParts0 * numParts1)
+    for (key0 <- Seq(1, 0, 10, "a", "ab", ("c", 5));
+         key1 <- Seq("some", -1, 0, ("d", "e"))) {
+      val partition = gridPart.getPartition((key0, key1))
+      assert(partition === part0.getPartition(key0) + part0.numPartitions * part1.getPartition(key1))
+      assert(gridPart.indices(partition).toSeq === Seq(part0.getPartition(key0), part1.getPartition(key1)))
+    }
+  }
+}
